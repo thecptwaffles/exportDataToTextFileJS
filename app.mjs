fcede41267dfb732c2ea328ef70f9253f4979e30
Cade Humphreys <thecptwaffles@zohomail.com>
2022-08-29 18:56:20 -0600

    trying to make function to add text to text file

--- app.mjs
+++ app.mjs
@@ -1,8 +1,21 @@
-import { exportFile } from 'fs-browsers';
+// Requiring fs module in which 
+// writeFile function is defined. 
+    import * as fs from 'fs'; 
+  
+// Data which will write in a file. 
+let data = "Learning how to write in a file."
+  
 
 
-function cool() {
-    
-    exportFile("this string will be exported to txt file", { fileName: 'hello.txt' });
+function newFunction() {
 
+    let email = document.getElementById('input').value;
+
+    fs.writeFile('Output.txt', email, (err) => {
+
+        // In case of a error throw err. 
+        if (err)
+            throw err;
+    });
 }
+
